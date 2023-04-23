503ceea403ad0ba02c8ec6786c0268ec59e71fa2
Ankit <ankit.ap.9932@gmail.com>
2023-04-23 20:49:18 +0530

    initial commit

---
+++ 1-NAVBAR/navbar.js
@@ -0,0 +1,16 @@
+
+
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active')
+// })
+
+const mobile_nav=document.querySelector(".mobile-navbar");
+const nav_header=document.querySelector ("#header")
+mobile_nav.addEventListener('click',()=>toggleNavbar());
+
+const toggleNavbar=()=>{
+  nav_header.classList.toggle('active');
+}
